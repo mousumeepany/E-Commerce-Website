9be8c6f056628ffd1875109e711a2901cd9fd38b
mousumee2306 <mousumeedkl@gmail.com>
2022-07-08 12:37:00 +0530

    First Commit

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa5o7mcczrcS9DAw5d7',
+        issuer: 'https://dev-77838458.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
